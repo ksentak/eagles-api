cbef43e74c853a745a0254b672309a9d50f08d5a
Keaton Sentak <ksentak01@gmail.com>
2022-10-04 21:46:05 -0400

    Add data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  findAll(): string {
+    return 'This action returns all players';
   }
 }
