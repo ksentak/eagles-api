2aa3ba55f5adff5033e61c56733dba4026b7fd01
Keaton Sentak <ksentak01@gmail.com>
2025-05-12 16:49:10 -0400

    update cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from app.routes import routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://eagles-api.netlify.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routes.router, prefix="/api/v1")
